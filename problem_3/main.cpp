ec89628b3f5698c778479e8fba67fc720132531a
Vasil <Vasil_i_Nikolov@abv.bg>
2018-12-06 00:10:51 +0200

    Solving Problem_3

--- problem_3/main.cpp
+++ problem_3/main.cpp
@@ -4,6 +4,17 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    long long int num = 600851475143;
+    long long int largestPrime = 1;
+
+    while(num != 1) {
+        largestPrime++;
+        while(num%largestPrime == 0) {
+            num = num/largestPrime;
+        }
+    }
+
+    cout<<largestPrime<<endl;
+
     return 0;
 }
