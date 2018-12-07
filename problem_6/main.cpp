3df493cc8b270a0938cb5d733a65926d4913ff9e
Vasil <Vasil_i_Nikolov@abv.bg>
2018-12-07 02:22:27 +0200

    Solving problem_6

--- problem_6/main.cpp
+++ problem_6/main.cpp
@@ -4,6 +4,22 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    long int sumSqr = 0;
+    int sum = 0;
+    long int sqrSum;
+    long int diff;
+    const int ceil = 100;
+
+    for(int i=1; i<=ceil; i++) {
+        sum = sum + i;
+        sumSqr = sumSqr + i*i;
+    }
+
+    sqrSum = sum * sum;
+
+    diff = sqrSum - sumSqr;
+
+    cout<<diff<<endl;
+
     return 0;
 }
