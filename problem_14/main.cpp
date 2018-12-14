0b77b54401b7a6daa46e3a53bb1b66165ef4fe95
Vasil <Vasil_i_Nikolov@abv.bg>
2018-12-15 01:52:57 +0200

    Solving problem_14

--- problem_14/main.cpp
+++ problem_14/main.cpp
@@ -4,6 +4,44 @@ using namespace std;
 
 int main()
 {
+    const long int ceil = 1000000;
+    int arr[ceil];
+
+    long int max = 1;
+
+    int counter;
+    long int curr;
+
+    for(int i=1; i<ceil; i++) {
+        counter = 0;
+        curr = i;
+
+        while(curr != 1) {
+            counter++;
+            if(curr%2 == 0) {
+                curr = curr/2;
+                if(curr<i) {
+                    counter = counter + arr[curr];
+                    curr = 1;
+                }
+            }
+            else {
+                curr = curr*3 + 1;
+                if(curr<i) {
+                    counter = counter + arr[curr];
+                    curr = 1;
+                }
+            }
+        }
+        arr[i] = counter;
+        //cout<<counter<<endl;
+
+        if(arr[i] > arr[max]) {
+            max = i;
+        }
+    }
+
+    cout<<max<<endl;
 
     return 0;
 }
