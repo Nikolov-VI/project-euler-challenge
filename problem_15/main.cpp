d347c487e924b456c306431fa99892d5dfea8f94
Vasil <Vasil_i_Nikolov@abv.bg>
2018-12-15 02:58:26 +0200

    Solving problem_15

--- problem_15/main.cpp
+++ problem_15/main.cpp
@@ -2,8 +2,31 @@
 
 using namespace std;
 
+
+long long int choose(int n, int k) {
+    if(n < k) {
+        cout<<"n can't be smaller than k"<<endl;
+        return -1;
+    }
+    long long int result = 1;
+
+    for(int i=1; i<=k; i++) {
+        result = result * (n-k+i) / i;
+    }
+
+    return result;
+}
+
+
 int main()
 {
+    const int side = 20;
+    long long int  pathAmount;
+
+    // Using formula 2side choose side to represent pascal triangle row and position
+    pathAmount = choose(side*2, side);
+
+    cout<<pathAmount<<endl;
 
     return 0;
 }
