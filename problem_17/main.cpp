03e6797e29397dfb98f0758174b2c7efa4f5f73d
Vasil <Vasil_i_Nikolov@abv.bg>
2018-12-16 05:47:58 +0200

    Solving problem_17

--- problem_17/main.cpp
+++ problem_17/main.cpp
@@ -4,12 +4,32 @@ using namespace std;
 
 int main()
 {
+    // from 1 to 9;
+    int toTen = 36;
+    // from 10 to 12
+    int tenToTwelve = 15;
+    // from 13 to 19
+    int teens = 27 + 7*4;
+    // from 20 to 29
+    int twentyToThirty = 6*10 + toTen;
+    // from 30 to 99
+    int thirtyToHundred = (teens-1 - 7*2)*10 + toTen*7;
+    // from 1 to 99
+    int toHundred = toTen + tenToTwelve + teens + twentyToThirty + thirtyToHundred;
+    // from 100 to 999
+    long int hundredToThousand = 9*(100*7 + 99*3 + toHundred) + toTen;
+    // from 1 to 999
+    long int toThousand = toHundred + hundredToThousand;
+    // One Thousand
+    int thousand = 11;
 
+    // from 1 to 1000
+    long int letterCount = toThousand + thousand;
 
-
-    for(int i=0; i<10; i++){
-
-    }
-
+    cout<<"teens = "<<teens<<endl;
+    cout<<"twentyToThirty = "<<twentyToThirty<<endl;
+    cout<<"toHundred = "<<toHundred<<endl;
+    cout<<hundredToThousand<<endl;
+    cout<<letterCount<<endl;
     return 0;
 }
